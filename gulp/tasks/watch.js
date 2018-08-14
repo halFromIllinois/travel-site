d06c4e77423c4245554b4d94b764f3d80690ca17
Eric <eric@webdevdoyle.com>
2018-08-14 22:23:40 +0200

    updated

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browsersync = require('browser-sync').create();
+
+
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/master.css')
+        .pipe(browsersync.stream());
+});
+
+
+gulp.task('watch', function(){
+
+    browsersync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function(){
+        browsersync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('cssInject');
+    });
+
+});
